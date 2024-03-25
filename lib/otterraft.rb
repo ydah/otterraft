30d3c3a247efab2424aa2d4c55698c9611de99e9
ydah <t.yudai92@gmail.com>
2024-03-25 18:57:31 +0900

    Initial commit

---
+++ lib/otterraft.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "otterraft/version"
+
+module Otterraft
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/otterraft/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Otterraft
+  VERSION = "0.1.0"
+end
